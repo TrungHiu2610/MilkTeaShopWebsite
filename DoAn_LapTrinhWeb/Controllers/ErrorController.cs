0a7eedc26f1e05d5ea240338d17141f79ca1dd4e
Trung Hiu <Trung Hiu@TRUNGHIU>
2024-11-08 15:23:33 +0700

    Add project files.

---
+++ DoAn_LapTrinhWeb/Controllers/ErrorController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAn_LapTrinhWeb.Controllers
+{
+    public class ErrorController : Controller
+    {
+        //
+        // GET: /Error/
+
+        public ActionResult NotFound()
+        {
+            Response.StatusCode = 404; // Thiết lập mã trạng thái là 404
+            return View();
+        }
+    }
+}
---
+++ DoAn_LapTrinhWeb/DoAn_LapTrinhWeb/Controllers/NhanVienController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAn_LapTrinhWeb.Controllers
+{
+    public class NhanVienController : Controller
+    {
+        //
+        // GET: /NhanVien/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
